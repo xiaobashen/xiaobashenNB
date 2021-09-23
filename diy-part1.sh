f3cb3c19d3f8e1f26728b14b3d50c438ac95a3b1
xiaobashen <90694432+xiaobashen@users.noreply.github.com>
2021-09-23 08:19:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,11 +8,15 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#更换测试内核
+#Update Kernel
 sed -i '13d; s/KERNEL_TESTING_PATCHVER/KERNEL_PATCHVER/g' ./target/linux/x86/Makefile
+
+#Zh-Netdata
+rm -rf ./package/lean/luci-app-netdata
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+git clone https://github.com/Jason6111/luci-app-netdata ./package/lean/luci-app-netdata
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
