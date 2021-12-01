3a31a1432851c55ae757902bd835d7e88c12f782
xiaobashen <90694432+xiaobashen@users.noreply.github.com>
2021-12-01 19:49:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #Update Kernel
-sed -i '13d; s/KERNEL_TESTING_PATCHVER/KERNEL_PATCHVER/g' ./target/linux/x86/Makefile
+sed -i '/KERNEL_PATCHVER=/ d; s/KERNEL_TESTING_PATCHVER/KERNEL_PATCHVER/g' ./target/linux/x86/Makefile
 
 #Zh-Netdata
 rm -rf ./package/lean/luci-app-netdata
